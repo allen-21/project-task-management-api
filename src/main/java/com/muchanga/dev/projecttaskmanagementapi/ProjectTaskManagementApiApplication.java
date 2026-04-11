03656a190f68a03917c5b40f343b72b1fc86d271
allen-21 <valteranibal1@gmail.com>
2026-04-11 16:05:28 +0200

    initial commit

---
+++ src/main/java/com/muchanga/dev/projecttaskmanagementapi/ProjectTaskManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.muchanga.dev.projecttaskmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTaskManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectTaskManagementApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/muchanga/dev/projecttaskmanagementapi/ProjectTaskManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muchanga.dev.projecttaskmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTaskManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
