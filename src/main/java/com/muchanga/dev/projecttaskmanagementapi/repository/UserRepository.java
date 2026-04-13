ffeb74c508b7b9af1b2249e8988d4437af5de5bf
allen-21 <valteranibal1@gmail.com>
2026-04-13 22:58:05 +0200

    refactor: update user repository

--- src/main/java/com/muchanga/dev/projecttaskmanagementapi/repository/UserRepository.java
+++ src/main/java/com/muchanga/dev/projecttaskmanagementapi/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package com.muchanga.dev.projecttaskmanagementapi.repository;
 
-public class UserRepository {
+import com.muchanga.dev.projecttaskmanagementapi.entity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByLogin(String login);
 }
