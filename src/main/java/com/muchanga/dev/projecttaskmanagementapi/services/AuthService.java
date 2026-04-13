f936b1db5cfa283ec8fb14824cf5fe36992c1834
allen-21 <valteranibal1@gmail.com>
2026-04-13 22:58:33 +0200

    refactor: update authentication service

--- src/main/java/com/muchanga/dev/projecttaskmanagementapi/services/AuthService.java
+++ src/main/java/com/muchanga/dev/projecttaskmanagementapi/services/AuthService.java
@@ -1,4 +1,29 @@
 package com.muchanga.dev.projecttaskmanagementapi.services;
 
+import com.muchanga.dev.projecttaskmanagementapi.dto.RegisterDTO;
+import com.muchanga.dev.projecttaskmanagementapi.entity.user.User;
+import com.muchanga.dev.projecttaskmanagementapi.exception.LoginAlreadyExistsException;
+import com.muchanga.dev.projecttaskmanagementapi.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void register(RegisterDTO data){
+        if(userRepository.findByLogin(data.login()).isPresent()){
+            throw new LoginAlreadyExistsException(data.login());
+        }
+        String encodedPassword = passwordEncoder.encode(data.password());
+        User newUser = new User(data.login(), encodedPassword);
+        userRepository.save(newUser);
+
+    }
 }
